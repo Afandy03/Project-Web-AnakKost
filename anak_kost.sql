17c62ee28cacdc8d58cde6cc51bb3a542f9e6757
egayastira <37532719+egayastira@users.noreply.github.com>
2018-06-04 07:05:59 +0700

    Add files via upload

--- anak_kost.sql
+++ anak_kost.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2018 at 04:55 AM
+-- Generation Time: Jun 03, 2018 at 04:25 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -42,7 +42,14 @@ CREATE TABLE `fasilitas` (
 --
 
 INSERT INTO `fasilitas` (`idFasilitas`, `wifi`, `barangDalam`, `kamarMandi`, `ac`, `idKost`) VALUES
-(7, 0, ' bed', 'luar', 0, 1);
+(7, 0, 'bed, almari pakaian, meja belajar, sekamar berdua\r\n        ', 'luar', 0, 1),
+(8, 1, 'bed, almari pakaian, meja belajar, sekamar berdua\r\n  ', 'dalam', 0, 2),
+(9, 0, ' bed, almari pakaian, meja beajar\r\n', 'luar', 0, 3),
+(10, 1, ' bed, almari pakaian, tv\r\n', 'dalam', 0, 4),
+(11, 0, ' bed, almari pakaian\r\n', 'luar', 0, 5),
+(12, 1, ' bed, lamari pakaian, tv\r\n', 'luar', 0, 6),
+(13, 0, ' bed, almari pakaian\r\n', 'luar', 0, 7),
+(14, 1, 'bed, almari pakaian, meja belajar,\r\n', 'dalam', 1, 8);
 
 -- --------------------------------------------------------
 
@@ -54,7 +61,7 @@ CREATE TABLE `kost` (
   `idKost` int(11) NOT NULL,
   `namaKost` varchar(20) NOT NULL,
   `alamat` varchar(30) NOT NULL,
-  `noTelp` int(15) NOT NULL,
+  `noTelp` bigint(15) NOT NULL,
   `namaPemilik` varchar(20) NOT NULL,
   `ukuran` varchar(10) NOT NULL,
   `deskripsi` text NOT NULL,
@@ -69,7 +76,14 @@ CREATE TABLE `kost` (
 --
 
 INSERT INTO `kost` (`idKost`, `namaKost`, `alamat`, `noTelp`, `namaPemilik`, `ukuran`, `deskripsi`, `jenis`, `tersedia`, `harga`, `gambar`) VALUES
-(1, 'nlkkmlkkm', 'mlkmklmklm', 69878797, 'jnknjknk', 'kmlm', 'mlkmmklk                    ', 'pria', 2, 450000, '1.jpg');
+(1, 'Kost Yurona', 'Jebres Surakarta', 81391423678, 'Sumarmo', '3.25 x 2.7', 'Batasan jam kunjung jam 10 malam, harga sudah termasuk listrik, ada petugas kebersihan\r\n           ', 'pria', 2, 450000, 'id1.jpg'),
+(2, 'Kost Putri krambil', 'Jebres Surakarta', 85263835412, 'Bu Warni', '3x4', 'Tidak ada batasan jam kunjung, tamu menginap tidak dikenai biaya tambahan\r\n   ', 'wanita', 5, 550000, 'id2.jpg'),
+(3, 'Kost Pak Parso', 'Jebres Surakarta', 81392456782, 'Pak Parso', '2x2.5', 'Tidak ada batasan jam kunjung, harga sudah termasuk listrik, ada petugas kebersihan kos, akses 24 jam, tamu menginap tidak ada tambahan biaya\r\n ', 'pria', 3, 350000, 'id3.jpg'),
+(4, 'Kost Qonita', 'Jebres Surakarta', 8567898734, 'Dewi Ratna', '2x3', 'Kost khusus putri muslim, listrik meteran tiap kamar\r\n', 'wanita', 4, 500000, 'id4.jpg'),
+(5, 'Kost Berkat Abadi 2', 'Jebres Surakarta', 8564201198, 'Roni dian', '3x4', 'Tambahan iuran biaya listrik jika membawa tv, komputer, lemari es dll\r\n ', 'pria', 2, 400000, 'id5.jpg'),
+(6, 'Kost Imarah', 'Jebres Surakarta', 8345672345, 'Yusuf Ardi', '3 x 3.5', 'Belum termasuk listrik, batasan jam kunjung sampai jam 10 malam, wifi membayar iuran\r\n ', 'pria', 3, 600000, 'id6.jpg'),
+(7, 'Kost Milla', 'Jebres Surakarta', 8976523421, 'Millaa', '3x3', 'Sudah termasuk listrik, membawa alat elektronik besar seperti kulkas ada tambahan biaya\r\n ', 'wanita', 2, 350000, 'id7.jpg'),
+(8, 'Kost Glory', 'Jebres Surakarta', 812345678, 'Gorraa', '3x4', 'tidak termasuk biaya listrik, hanya 500m dari pintu belakang UNS\r\n ', 'wanita', 3, 650000, 'id8.jpg');
 
 -- --------------------------------------------------------
 
@@ -89,7 +103,8 @@ CREATE TABLE `login` (
 
 INSERT INTO `login` (`Username`, `Password`, `Tipe`) VALUES
 ('fandy', 'fandy123', 'admin'),
-('userid1', 'user1', 'user');
+('userid1', 'user1', 'user'),
+('userid2', 'user2', 'user');
 
 -- --------------------------------------------------------
 
@@ -105,6 +120,13 @@ CREATE TABLE `order` (
   `idUser` int(11) NOT NULL,
   `idKost` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`idTransaksi`, `tglOrder`, `harga`, `status`, `idUser`, `idKost`) VALUES
+(2, '2018-06-03', 600000, 0, 2, 6);
 
 -- --------------------------------------------------------
 
@@ -126,7 +148,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`idUser`, `jenisKelamin`, `nama`, `email`, `noTelp`, `Username`) VALUES
-(1, 'pria', 'user nomer 1', 'user1@gmail.com', 627166156, 'userid1');
+(1, 'pria', 'user nomer 1', 'user1@gmail.com', 627166156, 'userid1'),
+(2, 'wanita', 'user nomber 2', 'user2@gmail.com', 99, 'userid2');
 
 -- --------------------------------------------------------
 
@@ -138,6 +161,15 @@ CREATE TABLE `wishlist` (
   `idKost` int(11) NOT NULL,
   `idUser` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `wishlist`
+--
+
+INSERT INTO `wishlist` (`idKost`, `idUser`) VALUES
+(1, 1),
+(1, 2),
+(6, 2);
 
 --
 -- Indexes for dumped tables
@@ -192,25 +224,25 @@ ALTER TABLE `wishlist`
 -- AUTO_INCREMENT for table `fasilitas`
 --
 ALTER TABLE `fasilitas`
-  MODIFY `idFasilitas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `idFasilitas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `kost`
 --
 ALTER TABLE `kost`
-  MODIFY `idKost` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idKost` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `idTransaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idTransaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
